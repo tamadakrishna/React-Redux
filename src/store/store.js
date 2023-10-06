f36572ea6fbd280025e7b947ab305479abe8b894
Krishna <tamadakrishnaa@gmail.com>
2023-10-06 16:24:17 +0530

    middleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterSlice from '../slices/counterSlice'
 
+const loggerMiddleware = store => next => action => {
+  console.log('logger middleware')
+  console.log('dispatching', action)
+  let result = next(action)
+  console.log('next state', store.getState())
+  return result
+}
+
 export default configureStore({
   reducer: {
     counter: counterSlice,
+    middleware:(getDefaultMiddleware)=>getDefaultMiddleware.concat(loggerMiddleware)
   },
 })
